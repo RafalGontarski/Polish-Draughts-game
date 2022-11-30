0b0582bc2ce5061b4bae39659bbaafcb168b94ed
Aneta Wawer <anwawer1@gmail.com>
2022-11-30 13:30:01 +0100

    get coordinates

--- src/main/java/com/codecool/polishdraughts/Game.java
+++ src/main/java/com/codecool/polishdraughts/Game.java
@@ -1,4 +1,23 @@
-package src.main.java.com;
+package com.codecool.polishdraughts;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Game {
+
+    public int[] getCoordinates(String message) {
+        int[] coordinates = new int[2];
+        String columns = "abcdefghijklmnopqrstuvwxyz";
+        Scanner pawn = new Scanner(System.in);
+
+        System.out.println(message);
+        String userInput = pawn.nextLine();
+        String column = userInput.substring(0, 1).toLowerCase();
+        int row = Integer.parseInt(userInput.substring(1));
+        coordinates[0] = columns.indexOf(column);
+        coordinates[1] = row - 1;
+        return coordinates;
+    }
+
+
 }
